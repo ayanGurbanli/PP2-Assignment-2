29c59a5148acead246bdb2fdc83ecb96f0b88599
ayanGurbanli <chappy_018@hotmail.com>
2023-11-30 12:40:46 +0400

    modified

--- MovieDatabase.java
+++ MovieDatabase.java
@@ -1,3 +1,11 @@
-public class MovieDatabase {
-    Movie[] movies;
+import java.util.ArrayList;
+import java.util.List;
+
+class MovieDatabase {
+    private List<Movie> movies;
+
+    public MovieDatabase() {
+        this.movies = new ArrayList<>();
+    }
+
 }
