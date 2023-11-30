25c616811dbe71e8c976899eabd419d151e7a7eb
ayanGurbanli <chappy_018@hotmail.com>
2023-11-30 14:06:25 +0400

    modified

--- MovieDatabase.java
+++ MovieDatabase.java
@@ -8,4 +8,13 @@ class MovieDatabase {
         this.movies = new ArrayList<>();
     }
 
+    public void addMovie (Movie newMovie){
+        movies.add(newMovie);
+    }
+    public void addMovies(List<Movie> newMovies) {
+        List<Movie> copy = List.copyOf(newMovies);
+        movies.addAll(copy);
+    }
+    
+
 }
